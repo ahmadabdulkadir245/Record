d02b53cb54c22d5677b6efe71cd9cfddda15a981
Ahmad Abdulkadir <dcomputernerd@gmail.com>
2022-06-02 19:44:49 -0700

    changing lowercase a to uppercase in d import for AllExpenses

--- pages/total.js
+++ pages/total.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import AllExpense from "../components/Main/allExpenses";
+import AllExpense from "../components/Main/AllExpenses";
 import { selectedExpense } from "../slices/expensesSlice";
 
 function Total() {
